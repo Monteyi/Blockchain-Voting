4bf9a33eef104df3e3df74d74c054e9b79332d66
Monteyi <79196763+Monteyi@users.noreply.github.com>
2024-08-14 15:14:07 +0200

    test + console  deploy works

--- foundry-fund-me/test/FundMeTest.t.sol
+++ foundry-fund-me/test/FundMeTest.t.sol
@@ -2,16 +2,25 @@
 
 pragma solidity ^0.8.19;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 
 // The is Test part indicates that FundMeTest is inheriting from the Test contract. 
 // In Solidity, inheritance allows a contract to inherit all the functions, variables, and modifiers from another contract. 
 // This means that FundMeTest can use all the functionalities provided by the Test contract without needing to redefine them.
 // These could include functions for setting up test environments, manipulating state, asserting conditions, and more.
 contract FundMeTest is Test {
-    function setUp() external {}
+    uint256 number = 1;
 
-    function testDemo() public {}
+    function setUp() external {
+        number = 2;
+    }
+
+    function testDemo() public {
+        console.log(number);
+        console.log("hello");
+        assertEq(number, 2);
+
+    }
 
 
 }

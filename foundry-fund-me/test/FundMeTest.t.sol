337dff9e84729fb07813735281b66d99af5115cb
Monteyi <79196763+Monteyi@users.noreply.github.com>
2024-08-14 15:46:57 +0200

    testOwnerIsMsgSender

--- foundry-fund-me/test/FundMeTest.t.sol
+++ foundry-fund-me/test/FundMeTest.t.sol
@@ -3,24 +3,27 @@
 pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
 
 // The is Test part indicates that FundMeTest is inheriting from the Test contract. 
 // In Solidity, inheritance allows a contract to inherit all the functions, variables, and modifiers from another contract. 
 // This means that FundMeTest can use all the functionalities provided by the Test contract without needing to redefine them.
 // These could include functions for setting up test environments, manipulating state, asserting conditions, and more.
 contract FundMeTest is Test {
-    uint256 number = 1;
+    FundMe fundMe;
 
-    function setUp() external {
-        number = 2;
-    }
+    function setUp()  external {
+        fundMe = new FundMe();
+        }
+// we use public view because Ensure that testMinimumDollarIsFive() only reads the contract’s state and does not modify it. it gives better clarity and better gas efficiency.
+    function testMinimumDolloarIsFive() public view{
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+        }
 
-    function testDemo() public {
-        console.log(number);
-        console.log("hello");
-        assertEq(number, 2);
-
-    }
-
-
+        function testOwnerIsMsgSender() public view{
+            console.log(fundMe.i_owner());
+            console.log(msg.sender);
+            console.log(address(this));
+            assertEq(fundMe.i_owner(), address(this));
+        }
 }
